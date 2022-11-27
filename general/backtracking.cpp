e76f870cea6d3332373789a17c39a8a1c67b4ab0
Kostya Farber <kostya.farber@gmail.com>
2022-11-27 16:55:35 +0000

    add hoares partition

---
+++ general/backtracking.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> ans;
+
+    void backtrack(int start, vector<int>& res, int n, int k) {
+        if (res.size() == k) {
+            print(res);
+            ans.push_back(res);
+            return;
+        }
+
+        for (int i = start; i < n + 1; i++) {
+            res.push_back(i);
+            backtrack(i + 1, res, n, k);
+            res.pop_back();
+        }
+    }
+
+    vector<vector<int>> combine(int n, int k) {
+        vector<int> current;
+        backtrack(1, current, n, k);
+        return ans;
+    }
+
+    void print(const vector<int>& res) {
+        for (auto &num: res) {
+            cout << num << ", ";
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+    int n = 4;
+    int k = 2;
+
+    Solution s;
+
+    vector<vector<int>> result = s.combine(4, 2);
+
+    return 0;
+}
--- general/partition_sort.cpp
+++ general/partition_sort.cpp
@@ -32,19 +32,45 @@ int partition(int start, int end, vector<int>& vec) {
     return i;
 }
 
+int h_partition(int start, int end, vector<int>& vec) {
+    int pivot = vec.at(start);
+    int i = start - 1;
+    int j = end + 1;
+
+    while (1) {
+        do {
+            i++;
+        } while (vec.at(i) < pivot);
+
+        do {
+            j--;
+        } while (vec.at(j) > pivot);
+        
+
+        if (i >= j) {
+            return j;
+        }
+
+        swap(vec.at(i), vec.at(j));
+    }  
+}
+
 void partition_sort(int start, int end, vector<int>& vec) {
     if (start >= end) {
         return;
     }
 
-    int p = partition(start, end, vec);
-    partition_sort(start, p - 1, vec);
+    // int p = partition(start, end, vec);
+    // partition_sort(start, p - 1, vec);
+    // partition_sort(p + 1, end, vec);
+    int p = h_partition(start, end, vec);
+    partition_sort(start, p, vec);
     partition_sort(p + 1, end, vec);
 
 }
 
 int main() {
-    vector<int> test = {5, 1, 2, 7, 6, 4};
+    vector<int> test = {5, 1, 2, 7, -2, 0, 6};
     print(test);
     partition_sort(0, test.size() - 1, test);
     print(test);
