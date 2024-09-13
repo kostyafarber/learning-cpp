ae1f6f493c50c3e2f63fd9a0440d92e848eccf95
Kostya Farber <kostya.farber@gmail.com>
2024-09-13 16:11:10 +0100

    trim the fat

--- general/backtracking.cpp
+++ general/backtracking.cpp
@@ -4,39 +4,46 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
     vector<vector<int>> ans;
-
-    void backtrack(int start, vector<int>& res, int n, int k) {
-        if (res.size() == k) {
+    void backtrack(int start, vector<int> &res, int n, int k)
+    {
+        if (res.size() == k)
+        {
             print(res);
             ans.push_back(res);
             return;
         }
 
-        for (int i = start; i < n + 1; i++) {
+        for (int i = start; i < n + 1; i++)
+        {
             res.push_back(i);
             backtrack(i + 1, res, n, k);
             res.pop_back();
         }
     }
 
-    vector<vector<int>> combine(int n, int k) {
+    vector<vector<int>> combine(int n, int k)
+    {
         vector<int> current;
         backtrack(1, current, n, k);
         return ans;
     }
 
-    void print(const vector<int>& res) {
-        for (auto &num: res) {
+    void print(const vector<int> &res)
+    {
+        for (auto &num : res)
+        {
             cout << num << ", ";
         }
         cout << endl;
     }
 };
 
-int main() {
+int main()
+{
     int n = 4;
     int k = 2;
 
